49d9f05b6b2f08bf1aad6aa53c9b5aa8d72ef22c
kwangmyung Kim <kmkim@whatap.io>
2017-02-04 22:03:32 +0900

    change package instal type to ruby

--- home/chef-repo/cookbooks/chef-solo/recipes/default.rb
+++ home/chef-repo/cookbooks/chef-solo/recipes/default.rb
@@ -12,12 +12,41 @@ execute "apt-get-update-periodic" do
   command "apt-get update"
   ignore_failure true
 end
+
 #install package
-package ["vim","htop"] do
-	action :install
+%w{nano vim htop build-essential git-core openssl libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config}.each do |pkg|
+	package pkg do
+		action :install
+	end
 end
+
+# package ["vim","htop","build-essential""] do
+# 	action :install
+# end
 
 #install knife-solo
 gem_package "knife-solo" do
 	action :install
+end
+
+#add .chef directory
+directory '/root/.chef' do
+	owner 'root'
+	group 'root'
+	mode '0751'
+	action :create
+end
+
+# add knife.rb file
+file "/root/.chef/knife.rb" do
+	owner "root"
+	group "root"
+	mode 00644
+	action :create
+end
+
+# change localtime to Seoul
+execute "change-time to Seoul" do
+  command "ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime"
+  ignore_failure true
 end
